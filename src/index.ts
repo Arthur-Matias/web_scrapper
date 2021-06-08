4003670cefdf138186463d8c0416a9a46b09f5a4
Arthur-Matias <ahmmfdc@gmail.com>
2021-06-08 15:42:46 -0300

    custom url via parameter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 import axios, { AxiosInstance } from "axios"
 
-const url:string = "[URL_HERE]";
+
 const axiosInstance:AxiosInstance = axios.create();
+const urlRegex: RegExp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
+class Params{
+    private options: string[] = [];
+    private url: string;
+    private args: string[] = process.argv.slice(2);
 
-axiosInstance.get(url)
+    constructor(){
+        for (let i = 0; i < this.args.length; i++) {
+            const element: string = this.args[i];
+            if (urlRegex.test(element)) {
+                this.url = element;
+            }else{
+                this.options.push(element);
+            }
+        }
+    }
+
+    getUrl = ():string => this.url;
+    getParams = ():string[] => this.options;
+
+    hasParams = () => this.options !== [];
+    hasUrl = () => this.url !== undefined;
+}
+
+const params = new Params();
+
+console.log(`has params: ${params.hasParams()}`);
+console.log(`has url: ${params.hasUrl()}`);
+console.log(params.getUrl());
+console.log(params.getParams());
+    
+axiosInstance.get(params.getUrl())
     .then(res => {
         const html = res.data;
         console.log(html);
     })
     .catch(console.error);
-
-
-console.log("Running ..."+ process.argv[2])
